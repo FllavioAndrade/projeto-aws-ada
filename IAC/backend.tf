a6a8851c5cbb0f119821209c3618ae4cc9f6a6b7
FllavioAndrade <flavio.a.s@live.com>
2024-12-08 20:38:59 -0300

    teste

--- IAC/backend.tf
+++ IAC/backend.tf
@@ -1,21 +1,5 @@
-# # Data source para verificar se o bucket existe
-# data "aws_s3_bucket" "backend" {
-#   count  = fileexists("${path.module}/backend_initialized") ? 1 : 0
-#   bucket = "terraform-state-ada-1182"
-# }
-
-# # Habilita backend S3 somente após a primeira aplicação
-# terraform {
-#   backend "s3" {
-#     bucket = "terraform-state-ada-1182"
-#     key    = "terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
-
-# # Recurso local para marcar que o backend foi inicializado
-# resource "local_file" "backend_initialized" {
-#   count    = fileexists("${path.module}/backend_initialized") ? 0 : 1
-#   filename = "${path.module}/backend_initialized"
-#   content  = "Backend initialized at ${timestamp()}"
-# }
+terraform {
+  backend "s3" {
+    # As configurações serão fornecidas via -backend-config no pipeline
+  }
+}
