4a40123e27546dcf5156e6b5d96bdec85cda4c2c
FllavioAndrade <flavio.a.s@live.com>
2024-12-09 00:18:47 -0300

    adicionado os demais recursos

--- IAC/rds.tf
+++ IAC/rds.tf
@@ -1,31 +1,84 @@
-# RDS Instance
-resource "aws_db_instance" "contabil" {
-  identifier           = "ada-contabil-db"
-  engine              = "postgres"
-  engine_version      = "15.4"  # Versão mais estável
-  instance_class      = "db.t3.micro"
-  allocated_storage   = 20
-  skip_final_snapshot = true
+# Lambda Function
+resource "aws_lambda_function" "processa_arquivo" {
+  filename         = "../app/lambda_function.zip"
+  function_name    = "process-arquivo-contabil"
+  role            = aws_iam_role.lambda_role.arn
+  handler         = "lambda_function.lambda_handler"
+  runtime         = "python3.8"
+  timeout         = 30
+  memory_size     = 256  # Aumentado para ter mais recursos
 
-  db_name  = "contabil"
-  username = "dbadmin"
-  password = "senhateste"  # Melhor usar secrets manager em produção
-
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
-  db_subnet_group_name   = aws_db_subnet_group.rds.name
-
-  # Desabilita acesso público
-  publicly_accessible = false
-  
-  # Habilita backup
-  #backup_retention_period = 7
-  
-  # Habilita monitoramento básico
-  monitoring_interval = 0
-  
-  tags = {
-    Name    = "RDS Contabil"
-    Projeto = "Ada Contabilidade"
-    dono    = "Flavio de Andrade"
+  environment {
+    variables = {
+      DB_HOST     = aws_db_instance.contabil.endpoint
+      DB_NAME     = aws_db_instance.contabil.db_name
+      DB_USER     = aws_db_instance.contabil.username
+      DB_PASSWORD = aws_db_instance.contabil.password
+    }
   }
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_1.id, aws_subnet.private_2.id]  # Usando duas subnets para redundância
+    security_group_ids = [aws_security_group.lambda_sg.id]
+  }
+}
+
+# IAM Role para Lambda
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_process_file_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Política básica de execução da Lambda
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Política para acesso à VPC
+resource "aws_iam_role_policy_attachment" "lambda_vpc" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+# Política personalizada para a Lambda
+resource "aws_iam_role_policy" "lambda_policy" {
+  name = "lambda_process_file_policy"
+  role = aws_iam_role.lambda_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:ListBucket",
+          "sqs:ReceiveMessage",
+          "sqs:DeleteMessage",
+          "sqs:GetQueueAttributes",
+          "sns:Publish"
+        ]
+        Resource = [
+          "${aws_s3_bucket.bucket_contabil.arn}",
+          "${aws_s3_bucket.bucket_contabil.arn}/*",
+          aws_sqs_queue.processa_arquivo.arn,
+          aws_sns_topic.upload_do_arquivo.arn
+        ]
+      }
+    ]
+  })
 }
